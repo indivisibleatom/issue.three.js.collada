ac0d5bda8f1d9c3a958263d180911cfe1f5980c8
Maël Nison <nison.mael@gmail.com>
2012-04-22 14:26:38 +0200

    Adds keyframe animations - not working

--- test.js
+++ test.js
@@ -1,31 +1,30 @@
 (function (Three) {
-	
+
 	var renderer = new Three.WebGLRenderer();
 	renderer.domElement.style.background = '#888';
 	renderer.domElement.style.position = 'absolute';
 	renderer.domElement.style.left = 0;
 	renderer.domElement.style.top = 0;
-	
+
 	var scene = new Three.Scene();
 	scene.add(new Three.AxisHelper());
-	
+
 	var camera = new Three.PerspectiveCamera(60, 1, .1, 10000);
 	camera.position.x = camera.position.y = camera.position.z = 1000;
 	camera.lookAt(new Three.Vector3(0, 0, 0));
 	scene.add(camera);
-	
+
 	var light = new Three.AmbientLight(0xdddddd);
 	light.position = camera.position;
 	scene.add(light);
-	
-	var skins = [];
-	
+
+	var animations = [];
+
 	new Three.ColladaLoader().load('export.dae', function (collada) {
-		skins = collada.skins;
-		
 		collada.scene.rotation.x = Math.PI;
-		
-		Three.SceneUtils.traverseHierarchy(collada.scene, function (node) {
+
+		// Fix materials
+		0 && Three.SceneUtils.traverseHierarchy(collada.scene, function (node) {
 			var material;
 			if ((material = node.material)) {
 				node.material = new Three.MeshBasicMaterial({
@@ -33,29 +32,30 @@
 				});
 			}
 		});
-		
+
+		// Set animations
+		var animationHandler = Three.AnimationHandler;
+		collada.animations.forEach(function (animationData) {
+			animationHandler.add(animationData);
+
+			var animation = new Three.KeyFrameAnimation(animationData.node, animationData.name);
+			animation.timeScale = 1;
+			animation.play(false, 0);
+			animations.push(animation);
+		});
+
 		scene.add(collada.scene);
 	});
-	
-	function morph(skin, frame) {
-		var influences = skin.morphTargetInfluences;
-		var influencesCount = influences.length;
-		for (var t = 0; t < influencesCount; ++ t) influences[t] = 0;
-		influences[Math.floor(frame) % influencesCount] = 1;
-	}
-	
-	var frame = 0, fps = 15;
+
 	var clock = new Three.Clock();
 	(function animate() {
-		var currentFrame = Math.floor(frame);
-		for (var t = 0, T = skins.length; t < T; ++t)
-			morph(skins[t], currentFrame);
-		frame = frame + clock.getDelta() * fps;
 		window.requestAnimationFrame(animate);
+		var delta = clock.getDelta();
+		animations.forEach(function (a) { a.update(delta); });
 		renderer.render(scene, camera);
 	}());
-	
+
 	window.onload = function () { window.onresize(); document.body.appendChild(renderer.domElement); };
 	window.onresize = function () { renderer.setSize(window.innerWidth, window.innerHeight); camera.aspect = window.innerWidth / window.innerHeight; camera.updateProjectionMatrix(); };
-	
+
 })(THREE);
