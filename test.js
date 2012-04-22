f127362c971c4306f12fbcf401bc21e0a4d1083e
Maël Nison <nison.mael@gmail.com>
2012-04-22 11:32:10 +0200

    Fixes visibility issue

--- test.js
+++ test.js
@@ -14,21 +14,30 @@
 	camera.lookAt(new Three.Vector3(0, 0, 0));
 	scene.add(camera);
 	
-	/* // Add a '/' at the start of this line to switch activated blocks
-	    var loader = new Three.JSONLoader();
-	    loader.load('export.js', function (geometry) {
-			var mesh = new Three.Mesh(geometry, new Three.MeshBasicMaterial({ color : 0xffffff }));
-			scene.add(mesh);
-		});
-	/*/
-	    var loader = new Three.ColladaLoader();
-	    loader.convertUpAxis = true;
-	    loader.load('export.dae', function (collada) {
-			scene.add(collada.scene);
-		});
-	//*/
+	var light = new Three.PointLight(0x444444);
+	light.position = camera.position;
+	scene.add(light);
 	
+	var model = null, skins = [], loader = new Three.ColladaLoader();
+	loader.load('export.dae', function (collada) {
+		model = collada.scene;
+		skins = collada.skins;
+		collada.scene.rotation.x = Math.PI;
+		scene.add(collada.scene);
+	});
+	
+	function morph(skin, percent) {
+		var influences = skin.morphTargetInfluences;
+		var influencesCount = influences.length;
+		for (var t = 0; t < influencesCount; ++ t) influences[t] = 0;
+		influences[Math.floor(percent * influencesCount)] = 1;
+	}
+	
+	var frame = 0, speed = 1/30;
 	(function animate() {
+		for (var t = 0, T = skins.length; t < T; ++t)
+			morph(skins[t], frame);
+		frame = (frame + speed) % 1;
 		window.requestAnimationFrame(animate);
 		renderer.render(scene, camera);
 	}());
